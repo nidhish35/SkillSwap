683ef96c19b786c085a72dd5df2d48f6a4fdfacc
nidhish35 <malavnidhish35@gmail.com>
2025-08-25 23:42:21 +0530

    Starting from Scratch

--- Server/utils/jwt.js
+++
@@ -1,4 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { env } from '../config/env.js';
-export const sign = (payload, exp='15m') => jwt.sign(payload, env.jwtSecret, { expiresIn: exp });
-export const verify = (token) => jwt.verify(token, env.jwtSecret);

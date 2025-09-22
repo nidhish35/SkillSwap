a8f262ebfe593e482e6a1067a35c2bb85278ac8c
nidhish35 <malavnidhish35@gmail.com>
2025-09-22 17:45:52 +0530

    changing localhost

--- Server/server.js
+++ Server/server.js
@@ -73,7 +73,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", // React frontend
+        origin: '*', // React frontend
         methods: ["GET", "POST"],
         credentials: true, // allow cookies
     },
